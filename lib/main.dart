4ed7a313de0fcfb82bf3a625aeb0ec91f41aebd4
MrRomainzZ <roman.shalin87@gmail.com>
2024-03-17 03:53:09 +0800

    Parse That JSON + some helper files

--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,71 @@
-import 'package:flutter/material.dart';
+import 'dart:async';
+import 'dart:convert';
 
-void main() {
-  runApp(const MainApp());
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+
+import 'models.dart';
+
+Future<Manifest> fetchManifest() async {
+	final response = await http
+			.get(Uri.parse('https://piston-meta.mojang.com/mc/game/version_manifest_v2.json'));
+
+	if (response.statusCode == 200) {
+		// If the server did return a 200 OK response,
+		// then parse the JSON.
+		return Manifest.fromJson(jsonDecode(response.body));
+	} else {
+		// If the server did not return a 200 OK response,
+		// then throw an exception.
+		throw Exception('Failed to load manifest');
+	}
 }
 
-class MainApp extends StatelessWidget {
-  const MainApp({super.key});
+void main() => runApp(const MyApp());
 
-  @override
-  Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
-        ),
-      ),
-    );
-  }
+class MyApp extends StatefulWidget {
+	const MyApp({super.key});
+
+	@override
+	State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+	late Future<Manifest> futureManifest;
+
+	@override
+	void initState() {
+		super.initState();
+		futureManifest = fetchManifest();
+	}
+
+	@override
+	Widget build(BuildContext context) {
+		return MaterialApp(
+			title: 'Fetch Data Example',
+			theme: ThemeData(
+				colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+			),
+			home: Scaffold(
+				appBar: AppBar(
+					title: const Text('Fetch Data Example'),
+				),
+				body: Center(
+					child: FutureBuilder<Manifest>(
+						future: futureManifest,
+						builder: (context, snapshot) {
+							if (snapshot.hasData) {
+								return Text(snapshot.data!.latest.release);
+							} else if (snapshot.hasError) {
+								return Text('${snapshot.error}');
+							}
+
+							// By default, show a loading spinner.
+							return const CircularProgressIndicator();
+						},
+					),
+				),
+			),
+		);
+	}
 }
